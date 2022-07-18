03e555e48d6ea8a12d19d5624230205893559c0a
caduce3 <74209989+caduce3@users.noreply.github.com>
2022-07-18 13:24:20 -0300

    Criando primeiro componente (banner) e modificando CSS do mesmo

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,6 +1,10 @@
+import './Banner.css';
+
 function Banner() {
     return (
-        <img src=""/>
+        <header className="banner">
+            <img src="imagens/banner.png" alt="Banner principal da página"/>
+        </header>
     )
 }
 export default Banner
