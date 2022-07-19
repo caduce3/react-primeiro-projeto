da8e28ae2a667eec32a397d2e8185b3d675ab30e
caduce3 <74209989+caduce3@users.noreply.github.com>
2022-07-19 16:53:54 -0300

    finalizando (manipulando campo do usuário)

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -1,12 +1,20 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+    
+   let valor = ''
+    
+    const aoDigitar = (evento) => {
+        valor = evento.target.value //guardando a referencia numa variável
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}></input>
+            <input value ={valor} onChange={aoDigitar} required={props.obrigatorio} placeholder={props.placeholder}></input>
         </div>
     )
 }
