e180f6244f5a5e25c91b95012656f3dd099a5b23
caduce3 <74209989+caduce3@users.noreply.github.com>
2022-07-20 17:02:57 -0300

    Iniciando componente colaborador estáticamente

---
+++ organo/src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Card.css'
+
+const Colaborador = () => {
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src="https://github.com/caduce3.png" alt="Cadu Lucena"/>
+            </div>
+            <div className='rodape'>
+                <h4>Cadu Lucena</h4>
+                <h5>cadu.ce3@gmail.com</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
